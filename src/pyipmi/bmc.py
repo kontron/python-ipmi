584f85e1f62690ad6127b04fecb9794082845c51
Michael Walle <michael@walle.cc>
2011-03-18 15:50:51 +0100

    get_device_id() fixes
    Use new GetDeviceId message format. Make returned object a little bit more
    pythonic.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

--- src/pyipmi/bmc.py
+++ src/pyipmi/bmc.py
@@ -26,8 +26,8 @@ class Helper:
         check_completion_code(m.rsp.completion_code)
 
 class DeviceId:
-    def __init__(self, rsp):
-        if rsp:
+    def __init__(self, rsp=None):
+        if rsp is not None:
             self.from_response(rsp)
 
     def __str__(self):
@@ -36,31 +36,51 @@ class DeviceId:
         s+= ' available: %d' % self.available
         s+= ' fw version: %d.%d' % (self.major_fw_revision, self.minor_fw_revision)
         s+= ' ipmi: %d' % self.ipmi_version
-        s+= ' manuf: %d' % self.manufacturer_id
+        s+= ' manufacturer: %d' % self.manufacturer_id
         s+= ' product: %d' % self.product_id
         return s
+
+    def supports_function(self, name):
+        """Returns if a function is supported.
+
+        `name` is one of 'SENSOR', 'SDR_REPOSITORY', 'SEL', 'FRU_INVENTORY',
+        'IPMB_EVENT_RECEIVER', 'IPMB_EVENT_GENERATOR', 'BRIDGE', 'CHASSIS'.
+        """
+
+        return name.lower() in self.supported_functions
 
     def from_response(self, rsp):
         self.id = rsp.device_id
         self.revision = rsp.device_revision.device_revision
-        self.provides_sdrs = rsp.device_revision.provides_device_sdrs
-        self.available = rsp.firmware_revision.device_available
-        self.major_fw_revision = rsp.firmware_revision.major_revision
-        self.minor_fw_revision = rsp.minor_firmware_revision
-        self.ipmi_version = rsp.ipmi_version
-
-        self.support_sensor = rsp.additional_support.sensor
-        self.support_sdrr = rsp.additional_support.sdr_repository
-        self.support_sel = rsp.additional_support.sel
-        self.support_fru = rsp.additional_support.fru_inventory
-        self.support_event_receiver = rsp.additional_support.ipmb_event_receiver
-        self.support_event_generator = rsp.additional_support.ipmb_event_generator
-        self.support_bridge = rsp.additional_support.bridge
-        self.support_chassis = rsp.additional_support.chassis
+        self.provides_sdrs = bool(rsp.device_revision.provides_device_sdrs)
+        self.available = bool(rsp.firmware_revision.device_available)
+        self.major_fw_revision = rsp.firmware_revision.major
+        self.minor_fw_revision = rsp.firmware_revision.minor
+        self.major_ipmi_version = rsp.ipmi_version & 0xf
+        self.minor_ipmi_version = (rsp.ipmi_version >> 4) & 0xf
 
         self.manufacturer_id = rsp.manufacturer_id
         self.product_id = rsp.product_id
-        self.aux0 = rsp.auxiliary_0
-        self.aux1 = rsp.auxiliary_1
-        self.aux2 = rsp.auxiliary_2
-        self.aux3 = rsp.auxiliary_3
+
+        self.supported_functions = []
+        if rsp.additional_support.sensor:
+            self.supported_functions.append('sensor')
+        if rsp.additional_support.sdr_repository:
+            self.supported_functions.append('sdr_repository')
+        if rsp.additional_support.sel:
+            self.supported_functions.append('sel')
+        if rsp.additional_support.fru_inventory:
+            self.supported_functions.append('fru_inventory')
+        if rsp.additional_support.ipmb_event_receiver:
+            self.supported_functions.append('ipmb_event_receiver')
+        if rsp.additional_support.ipmb_event_generator:
+            self.supported_functions.append('ipmb_event_generator')
+        if rsp.additional_support.bridge:
+            self.supported_functions.append('bridge')
+        if rsp.additional_support.chassis:
+            self.supported_functions.append('chassis')
+
+        if hasattr(rsp, 'auxiliary'):
+            self.aux = list(rsp.auxiliary)
+        else:
+            self.aux = None
