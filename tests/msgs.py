27d77059b77e7e1774ecc420b617321d75c2209f
Michael Walle <michael@walle.cc>
2011-03-18 15:50:33 +0100

    add unittest for Get Device Id message
    Signed-off-by: Michael Walle <michael@walle.cc>

--- tests/msgs.py
+++ tests/msgs.py
@@ -65,5 +65,31 @@ class TestReadFruData(unittest.TestCase):
         m.rsp.data = array('c', '\x01\x02')
         self.assertRaises(EncodingError, m.rsp.encode)
 
+
+class TestGetDeviceId(unittest.TestCase):
+    def test_decode_valid_res(self):
+        m = pyipmi.msgs.bmc.GetDeviceId()
+        m.rsp.decode('\x00\x0c\x89\x00\x00\x02\x3d\x98'
+                '\x3a\x00\xbe\x14\x04\x00\x02\x00')
+        self.assertEqual(m.rsp.completion_code, 0)
+        self.assertEqual(m.rsp.device_id, 0x0c)
+        self.assertEqual(m.rsp.device_revision.device_revision, 9)
+        self.assertEqual(m.rsp.device_revision.provides_device_sdrs, 1)
+        self.assertEqual(m.rsp.firmware_revision.major, 0)
+        self.assertEqual(m.rsp.firmware_revision.device_available, 0)
+        self.assertEqual(m.rsp.firmware_revision.minor, 0)
+        self.assertEqual(m.rsp.ipmi_version, 2)
+        self.assertEqual(m.rsp.additional_support.sensor, 1)
+        self.assertEqual(m.rsp.additional_support.sdr_repository, 0)
+        self.assertEqual(m.rsp.additional_support.sel, 1)
+        self.assertEqual(m.rsp.additional_support.fru_inventory, 1)
+        self.assertEqual(m.rsp.additional_support.ipmb_event_receiver, 1)
+        self.assertEqual(m.rsp.additional_support.ipmb_event_generator, 1)
+        self.assertEqual(m.rsp.additional_support.bridge, 0)
+        self.assertEqual(m.rsp.additional_support.chassis, 0)
+        self.assertEqual(m.rsp.manufacturer_id, 15000)
+        self.assertEqual(m.rsp.product_id, 5310)
+        self.assertEqual(m.rsp.auxiliary, array('B', [4, 0, 2, 0]))
+
 if __name__ == '__main__':
     unittest.main()
