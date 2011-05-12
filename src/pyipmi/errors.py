93885f0ec288fb90f4712c02f0f46e3c4dac049d
Michael Walle <michael.walle@kontron.com>
2011-05-12 13:38:54 +0200

    errors: pretty printing for CompletionCodeError
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- src/pyipmi/errors.py
+++ src/pyipmi/errors.py
@@ -14,6 +14,8 @@ class CompletionCodeError(Exception):
     def __init__(self, cc):
         self.cc = cc
 
+    def __str__(self):
+        return "%s cc=0x%02x" % (self.__class__.__name__, self.cc)
 
 class NotSupportedError(Exception):
     pass
