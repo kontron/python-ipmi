8977deeef4410feb96d7a8f3241850b104e652fd
Michael Walle <michael.walle@kontron.com>
2011-06-07 18:11:26 +0200

    fru: add fru decoding support (wip)
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- src/pyipmi/fru.py
+++ src/pyipmi/fru.py
@@ -6,10 +6,37 @@
 #
 
 import array
+import codecs
+import datetime
 
 from pyipmi.errors import DecodingError, CompletionCodeError
 from pyipmi.msgs import fru
 from pyipmi.utils import check_completion_code
+
+bcd_map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '-', '.' ]
+
+def bcd_encode(input, errors='strict'):
+    raise NotImplementedError()
+
+def bcd_decode(input, errors='strict'):
+    chars = list()
+    try:
+        for b in input:
+            b = ord(b)
+            chars.append(bcd_map[b>>4 & 0xf] + bcd_map[b & 0xf])
+        return (''.join(chars), len(input) * 2)
+    except IndexError:
+        raise ValueError()
+
+def bcd_search(name):
+    if name != 'bcd+':
+        return None
+    return codecs.CodecInfo(
+            name = 'bcd+',
+            encode = bcd_encode,
+            decode = bcd_decode)
+
+codecs.register(bcd_search)
 
 class Helper:
     def get_fru_inventory_area_info(self, fn, fru_id=0):
@@ -66,30 +93,181 @@ class Helper:
             data.extend(m.rsp.data)
             off += m.rsp.count
 
-        f = FruInventory(data.tostring())
-        return f
+        return data.tostring()
 
-class FruField:
-    TYPE_CODE_BINARY = 0
-    TYPE_CODE_BCD = 1
-    TYPE_CODE_6_BIT_ASCII = 2
-    TYPE_CODE_8_BIT_ASCII = 3
+    def get_fru_inventory(self, fn, fru_id=0):
+        return FruInventory(self.read_fru_data(fn, fru_id))
+
+class FruDataField:
+    TYPE_BINARY = 0
+    TYPE_BCD_PLUS = 1
+    TYPE_6BIT_ASCII = 2
+    TYPE_ASCII_OR_UTF16 = 3
+
+    def __init__(self, data=None, offset=0, force_lang_english=False):
+        if data:
+            self.from_data(data, offset, force_lang_english)
+
+    def __str__(self):
+        return 'FruDataField(type=%d,len=%d,val=%s)' % (
+                self.type, self.length, self.value)
+
+    def from_data(self, data, offset=0, force_lang_english=False):
+        self.type = ord(data[offset]) >> 6 & 0x3
+        self.length = ord(data[offset]) & 0x3f
+
+        self.raw = data[offset+1:offset+1+self.length]
+
+        if type == self.TYPE_BCD_PLUS:
+            value = self.raw.decode('bcd+')
+        elif type == self.TYPE_6BIT_ASCII:
+            value = self.raw.decode('6bitascii')
+        else:
+            value = self.raw
+
+        self.value = value
+
+class InventoryCommonHeader:
+    def __init__(self, data=None):
+        if data:
+            self.from_data(data)
+
+    def from_data(self, data):
+        if len(data) != 8:
+            raise DecodingError('InventoryCommonHeader length != 8')
+        self.format_version = ord(data[0]) & 0x0f
+        self.internal_use_area_offset = ord(data[1]) * 8 or None
+        self.chassis_info_area_offset = ord(data[2]) * 8 or None
+        self.board_info_area_offset = ord(data[3]) * 8 or None
+        self.product_info_area_offset = ord(data[4]) * 8 or None
+        self.multirecord_area_offset = ord(data[5]) * 8 or None
+        if sum([ord(c) for c in data]) % 256 != 0:
+            raise DecodingError('InventoryCommonHeader checksum failed')
+
+
+class CommonInfoArea:
+    def __init__(self, data=None):
+        if data:
+            self.from_data(data)
+
+    def from_data(self, data):
+        self.format_version = ord(data[0]) & 0x0f
+        if self.format_version != 1:
+            raise DecodingError('unsupported format version (%d)' %
+                    self.format_version)
+        self.length = ord(data[1]) * 8
+        if sum([ord(c) for c in data[:self.length]]) % 256 != 0:
+            raise DecodingError('checksum failed')
+
+class InventoryChassisInfoArea(CommonInfoArea):
+    TYPE_OTHER = 1
+    TYPE_UNKNOWN = 2
+    TYPE_DESKTOP = 3
+    TYPE_LOW_PROFILE_DESKTOP = 4
+    TYPE_PIZZA_BOX = 5
+    TYPE_MINI_TOWER = 6
+    TYPE_TOWER = 7
+    TYPE_PORTABLE = 8
+    TYPE_LAPTOP = 9
+    TYPE_NOTEBOOK = 10
+    TYPE_HAND_HELD = 11
+    TYPE_DOCKING_STATION = 12
+    TYPE_ALL_IN_ONE = 13
+    TYPE_SUB_NOTEBOOK = 14
+    TYPE_SPACE_SAVING = 15
+    TYPE_LUNCH_BOX = 16
+    TYPE_MAIN_SERVER_CHASSIS = 17
+    TYPE_EXPANSION_CHASSIS = 18
+    TYPE_SUB_CHASSIS = 19
+    TYPE_BUS_EXPANSION_CHASSIS = 20
+    TYPE_PERIPHERAL_CHASSIS = 21
+    TYPE_RAID_CHASSIS = 22
+    TYPE_RACK_MOUNT_CHASSIS = 23
+
+    def from_data(self, data):
+        CommonInfoArea.from_data(self, data)
+        self.type = ord(data[2])
+        offset = 3
+        self.part_number = FruDataField(data, offset)
+        offset += self.part_number.length+1
+        self.serial_number = FruDataField(data, offset, True)
+        offset += self.serial_number.length+1
+        self.custom_chassis_info = list()
+        while ord(data[offset]) != 0xc1:
+            field = FruDataField(data, offset)
+            self.custom_chassis_info.append(field)
+            offset += field.length+1
+
+class InventoryBoardInfoArea(CommonInfoArea):
+    def from_data(self, data):
+        CommonInfoArea.from_data(self, data)
+        self.language_code = ord(data[2])
+        minutes = ord(data[5]) << 16 | ord(data[4]) << 8 | ord(data[3])
+        self.mfg_date = (datetime.datetime(1996, 1, 1)
+                + datetime.timedelta(minutes=minutes))
+        offset = 6
+        self.manufacturer = FruDataField(data, offset)
+        offset += self.manufacturer.length+1
+        self.product_name = FruDataField(data, offset)
+        offset += self.product_name.length+1
+        self.serial_number = FruDataField(data, offset, True)
+        offset += self.serial_number.length+1
+        self.part_number = FruDataField(data, offset)
+        offset += self.part_number.length+1
+        self.fru_file_id = FruDataField(data, offset, True)
+        offset += self.fru_file_id.length+1
+        self.custom_mfg_info = list()
+        while ord(data[offset]) != 0xc1:
+            field = FruDataField(data, offset)
+            self.custom_mfg_info.append(field)
+            offset += field.length+1
+
+class InventoryProductInfoArea(CommonInfoArea):
+    def from_data(self, data):
+        CommonInfoArea.from_data(self, data)
+        self.language_code = ord(data[2])
+        offset = 3
+        self.manufacturer = FruDataField(data, offset)
+        offset += self.manufacturer.length+1
+        self.name = FruDataField(data, offset)
+        offset += self.name.length+1
+        self.part_number = FruDataField(data, offset)
+        offset += self.part_number.length+1
+        self.version = FruDataField(data, offset)
+        offset += self.version.length+1
+        self.serial_number = FruDataField(data, offset, True)
+        offset += self.serial_number.length+1
+        self.asset_tag = FruDataField(data, offset)
+        offset += self.asset_tag.length+1
+        self.fru_file_id = FruDataField(data, offset, True)
+        offset += self.fru_file_id.length+1
+        self.custom_mfg_info = list()
+        while ord(data[offset]) != 0xc1:
+            field = FruDataField(data, offset)
+            self.custom_mfg_info.append(field)
+            offset += field.length+1
 
 class FruInventory:
     def __init__(self, data=None):
+        self.chassis_info_area = None
+        self.board_info_area = None
+        self.product_info_area = None
+
         if data:
-            self.data = data
+            self.from_data(data)
 
-    def __str__(self):
-        s = ''
-        s += 'data len: %d\n' % len(self.data)
-        s += ' '.join( ["%02X" % ord(x) for x in self.data])
-        return s
+    def from_data(self, data):
+        self.raw = data
+        self.common_header = InventoryCommonHeader(data[:8])
 
-    def get_data(self, offset=None, count=None):
-        if not offset:
-            return self.data[:]
-        elif not count:
-            return self.data[offset:]
-        else:
-            return self.data[offset:offset+count]
+        if self.common_header.chassis_info_area_offset:
+            self.chassis_info_area = InventoryChassisInfoArea(
+                    data[self.common_header.chassis_info_area_offset:])
+
+        if self.common_header.board_info_area_offset:
+            self.board_info_area = InventoryBoardInfoArea(
+                    data[self.common_header.board_info_area_offset:])
+
+        if self.common_header.product_info_area_offset:
+            self.product_info_area = InventoryProductInfoArea(
+                    data[self.common_header.product_info_area_offset:])
