cab7247ce26229e74e40143ac893b257172161d7
Michael Walle <michael.walle@kontron.com>
2011-10-26 17:24:53 +0200

    bump to development version
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 def main():
     setup(name = 'pyipmi',
-            version = '3.00',
+            version = '3.99',
             description = 'Pure python IPMI library',
             author_email = 'michael.walle@kontron.com',
             package_dir = { '' : 'src' },
