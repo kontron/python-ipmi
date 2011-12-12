168e348c3fd00382e44c1064e54930d6feefa104
Michael Walle <michael.walle@kontron.com>
2011-12-12 11:26:46 +0100

    simplify source layout
    Get rid of the src/ directory.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def main():
     setup(name = 'pyipmi',
             version = '3.99',
             description = 'Pure python IPMI library',
             author_email = 'michael.walle@kontron.com',
-            package_dir = { '' : 'src' },
-            packages = [ 'pyipmi',
-                'pyipmi.interfaces',
-                'pyipmi.msgs',
-                'pyipmi.ext',
-                'pyipmi.ext.totalphase',
-            ],
+            packages = find_packages(exclude="test"),
             package_data = {
                 'pyipmi.ext.totalphase':
                     ['aardvark.so', 'LICENSE.txt']
             },
-            scripts = ['bin/ipmitool.py']
+            scripts = ['bin/ipmitool.py'],
     )
 
 if __name__ == '__main__':
