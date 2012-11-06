f6e6f0f3e90ece3854d4660b7b62fa5304d30f81
Heiko Thiery <heiko.thiery@kontron.com>
2012-11-06 14:45:16 +0100

    event: add set_event_receiver/get_event_receiver
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- pyipmi/event.py
+++ pyipmi/event.py
@@ -4,5 +4,27 @@
 # author: Heiko Thiery <heiko.thiery@kontron.com>
 # author: Michael Walle <michael.walle@kontron.com>
 #
+
+from pyipmi.errors import DecodingError, CompletionCodeError, RetryError
+from pyipmi.utils import check_completion_code, ByteBuffer
+from pyipmi.msgs import create_request_by_name
+from pyipmi.msgs import constants
+
 EVENT_ASSERTION = 0
 EVENT_DEASSERTION = 1
+
+class Event:
+    def set_event_receiver(self, ipmb_address, lun):
+        req = create_request_by_name('SetEventReceiver')
+        req.event_receiver.ipmb_i2c_slave_address = ipmb_address
+        req.event_receiver.lun = lun
+        rsp = self.send_message(req)
+        check_completion_code(rsp.completion_code)
+
+    def get_event_receiver(self):
+        req = create_request_by_name('GetEventReceiver')
+        rsp = self.send_message(req)
+        check_completion_code(rsp.completion_code)
+        ipmb_address = rsp.event_receiver.ipmb_i2c_slave_address
+        lun = rsp.event_receiver.lun
+        return (ipmb_address, lun)
