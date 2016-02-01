ea552ccfae7ec127d40f0117230d4932a53cd1e2
Heiko Thiery <heiko.thiery@kontron.com>
2016-02-01 10:57:48 +0100

    ipmb: add test case for encode_ipmb_msg
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- tests/interfaces/test_ipmb.py
+++ tests/interfaces/test_ipmb.py
@@ -6,7 +6,7 @@ from array import array
 import nose
 from nose.tools import eq_, ok_, raises
 
-from pyipmi.interfaces.ipmb import checksum, IpmbHeader
+from pyipmi.interfaces.ipmb import checksum, IpmbHeader, encode_ipmb_msg
 
 def test_checksum():
     eq_(checksum([1,2,3,4,5]), 256-15)
@@ -22,3 +22,20 @@ def test_encode():
     header.cmd_id = 1
     data = header.encode()
     eq_(data, array('B', [0x72, 0x18, 0x76, 0x20, 0x08, 0x01]))
+
+
+def test_encode_ipmb_msg():
+    header = IpmbHeader()
+    header.rs_lun = 0
+    header.rs_sa = 0x72
+    header.rq_seq = 2
+    header.rq_lun = 0
+    header.rq_sa = 0x20
+    header.netfn = 6
+    header.cmd_id = 1
+
+    eq_(encode_ipmb_msg(header, [0xaa,0xbb,0xcc]),
+            '\x72\x18\x76\x20\x08\x01\xaa\xbb\xcc\xa6')
+
+    eq_(encode_ipmb_msg(header, '\xaa\xbb\xcc'),
+            '\x72\x18\x76\x20\x08\x01\xaa\xbb\xcc\xa6')
