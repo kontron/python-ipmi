302bfaff13e50d6ed02e8bf52e8d138fe78e7b40
Heiko Thiery <heiko.thiery@kontron.com>
2016-02-01 10:42:15 +0100

    ipmb: add testcase
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- tests/interfaces/test_ipmb.py
+++ tests/interfaces/test_ipmb.py
@@ -6,17 +6,19 @@ from array import array
 import nose
 from nose.tools import eq_, ok_, raises
 
-from pyipmi.interfaces.ipmb import IpmbHeader
+from pyipmi.interfaces.ipmb import checksum, IpmbHeader
 
-class TestIpmb:
-    def test_encode(self):
-        header = IpmbHeader()
-        header.rs_lun = 0
-        header.rs_sa = 0x72
-        header.rq_seq = 2
-        header.rq_lun = 0
-        header.rq_sa = 0x20
-        header.netfn = 6
-        header.cmd_id = 1
-        data = header.encode()
-        eq_(data, array('B', [0x72, 0x18, 0x76, 0x20, 0x08, 0x01]))
+def test_checksum():
+    eq_(checksum([1,2,3,4,5]), 256-15)
+
+def test_encode():
+    header = IpmbHeader()
+    header.rs_lun = 0
+    header.rs_sa = 0x72
+    header.rq_seq = 2
+    header.rq_lun = 0
+    header.rq_sa = 0x20
+    header.netfn = 6
+    header.cmd_id = 1
+    data = header.encode()
+    eq_(data, array('B', [0x72, 0x18, 0x76, 0x20, 0x08, 0x01]))
