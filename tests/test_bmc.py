24439d318668897d8d1aff99df1606e80d45b875
Heiko Thiery <heiko.thiery@kontron.com>
2016-03-05 09:36:34 +0100

    tests: add watchdog test
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- tests/test_bmc.py
+++ tests/test_bmc.py
@@ -8,6 +8,22 @@ import pyipmi.msgs.bmc
 from pyipmi.msgs import encode_message
 from pyipmi.msgs import decode_message
 
+def test_watchdog_object():
+    m = pyipmi.msgs.bmc.GetWatchdogTimerRsp()
+    decode_message(m, '\x00\x41\x42\x33\x44\x55\x66\x77\x88')
+
+    w = Watchdog(m)
+    eq_(w.timer_use, 1)
+    eq_(w.is_running, 1)
+    eq_(w.dont_log, 0)
+    eq_(w.timeout_action, 2)
+    eq_(w.pre_timeout_interrupt, 4)
+    eq_(w.pre_timeout_interval, 0x33)
+
+    eq_(w.timer_use_expiration_flags, 0x44)
+    eq_(w.initial_countdown, 0x6655)
+    eq_(w.present_countdown, 0x8877)
+
 def test_deviceid_object():
     m = pyipmi.msgs.bmc.GetDeviceIdRsp()
     decode_message(m, '\x00\x12\x84\x05\x67\x51\x55\x12\x34\x56\x44\x55')
