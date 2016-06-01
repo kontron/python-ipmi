07197db38be0ade571af195a8f54af70a2c9f17f
[Teslafly] <marshscholz@gmail.com>
2016-06-01 15:59:57 -0500

    fixed mock tests for ipmitool to return bytes instead of strings

--- tests/interfaces/test_ipmitool.py
+++ tests/interfaces/test_ipmitool.py
@@ -23,7 +23,7 @@ class TestIpmitool:
 
     def test_send_and_receive_raw_valid(self):
         mock = MagicMock()
-        mock.return_value = ('', 0)
+        mock.return_value = (b'', 0)
         self._interface._run_ipmitool = mock
 
         target = Target(0x20)
@@ -36,7 +36,7 @@ class TestIpmitool:
         interface.establish_session(self.session)
 
         mock = MagicMock()
-        mock.return_value = ('', 0)
+        mock.return_value = (b'', 0)
         interface._run_ipmitool = mock
 
         target = Target(0x20)
@@ -46,7 +46,7 @@ class TestIpmitool:
 
     def test_send_and_receive_raw_no_auth(self):
         mock = MagicMock()
-        mock.return_value = ('', 0)
+        mock.return_value = (b'', 0)
         self._interface._run_ipmitool = mock
 
         self._interface._session.set_auth_type(Session.AUTH_TYPE_NONE)
@@ -58,7 +58,7 @@ class TestIpmitool:
 
     def test_send_and_receive_raw_return_value(self):
         mock = MagicMock()
-        mock.return_value = (' 10 80 01 02 51 bd 98 3a 00 a8 06 00 03 00 00\n', 0)
+        mock.return_value = (b' 10 80 01 02 51 bd 98 3a 00 a8 06 00 03 00 00\n', 0)
         self._interface._run_ipmitool = mock
 
         target = Target(0x20)
@@ -69,7 +69,7 @@ class TestIpmitool:
 
     def test_send_and_receive_raw_completion_code_timeout(self):
         mock = MagicMock()
-        mock.return_value = ('Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1 rsp=0xc3): Ignore Me\n', 1)
+        mock.return_value = (b'Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1 rsp=0xc3): Ignore Me\n', 1)
 
         target = Target(0x20)
         self._interface._run_ipmitool = mock
@@ -79,7 +79,7 @@ class TestIpmitool:
 
     def test_send_and_receive_raw_completion_code_not_ok(self):
         mock = MagicMock()
-        mock.return_value = ('Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1 rsp=0xcc): Ignore Me\n', 1)
+        mock.return_value = (b'Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1 rsp=0xcc): Ignore Me\n', 1)
 
         target = Target(0x20)
         self._interface._run_ipmitool = mock
@@ -90,7 +90,7 @@ class TestIpmitool:
     @raises(TimeoutError)
     def test_send_and_receive_raw_timeout_without_response(self):
         mock = MagicMock()
-        mock.return_value = ('Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1)\n', 1)
+        mock.return_value = (b'Unable to send RAW command (channel=0x0 netfn=0x6 lun=0x0 cmd=0x1)\n', 1)
 
         target = Target(0x20)
         self._interface._run_ipmitool = mock
