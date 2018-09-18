1fd53fea0113dbdf3352c0187a017a20b409c65d
Heiko Thiery <heiko.thiery@kontron.com>
2018-09-18 07:51:08 +0200

    fix in sdr capabilities decoding
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- pyipmi/sdr.py
+++ pyipmi/sdr.py
@@ -329,6 +329,53 @@ class SdrFullSensorRecord(SdrCommon):
             value = -(1 << size) + value
         return value
 
+    def _decode_capabilities(self, capabilities):
+        self.capabilities = []
+
+        # ignore sensor
+        if capabilities & 0x80:
+            self.capabilities.append('ignore_sensor')
+        # sensor auto re-arm support
+        if capabilities & 0x40:
+            self.capabilities.append('auto_rearm')
+        # sensor hysteresis support
+        HYSTERESIS_MASK = 0x30
+        HYSTERESIS_IS_NOT_SUPPORTED = 0x00
+        HYSTERESIS_IS_READABLE = 0x10
+        HYSTERESIS_IS_READ_AND_SETTABLE = 0x20
+        HYSTERESIS_IS_FIXED = 0x30
+        if capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_NOT_SUPPORTED:
+            self.capabilities.append('hysteresis_not_supported')
+        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_READABLE:
+            self.capabilities.append('hysteresis_readable')
+        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_READ_AND_SETTABLE:
+            self.capabilities.append('hysteresis_read_and_setable')
+        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_FIXED:
+            self.capabilities.append('hysteresis_fixed')
+        # sensor threshold support
+        THRESHOLD_MASK = 0x0C
+        THRESHOLD_IS_NOT_SUPPORTED = 0x00
+        THRESHOLD_IS_READABLE = 0x08
+        THRESHOLD_IS_READ_AND_SETTABLE = 0x04
+        THRESHOLD_IS_FIXED = 0x0C
+        if capabilities & THRESHOLD_MASK == THRESHOLD_IS_NOT_SUPPORTED:
+            self.capabilities.append('threshold_not_supported')
+        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_READABLE:
+            self.capabilities.append('threshold_readable')
+        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_READ_AND_SETTABLE:
+            self.capabilities.append('threshold_read_and_setable')
+        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_FIXED:
+            self.capabilities.append('threshold_fixed')
+        # sensor event message control support
+        if (capabilities & 0x03) is 0:
+            pass
+        if (capabilities & 0x03) is 1:
+            pass
+        if (capabilities & 0x03) is 2:
+            pass
+        if (capabilities & 0x03) is 3:
+            pass
+
     def _from_data(self, data):
         buffer = ByteBuffer(data[5:])
         # record key bytes
@@ -355,51 +402,7 @@ class SdrFullSensorRecord(SdrCommon):
             self.initialization.append('default_scanning')
 
         # byte 12 - sensor capabilities
-        capabilities = buffer.pop_unsigned_int(1)
-        self.capabilities = []
-        # ignore sensor
-        if capabilities & 0x80:
-            self.capabilities.append('ignore_sensor')
-        # sensor auto re-arm support
-        if capabilities & 0x40:
-            self.capabilities.append('auto_rearm')
-        # sensor hysteresis support
-        HYSTERESIS_MASK = 0x30
-        HYSTERESIS_IS_NOT_SUPPORTED = 0x00
-        HYSTERESIS_IS_READABLE = 0x10
-        HYSTERESIS_IS_READ_AND_SETTABLE = 0x20
-        HYSTERESIS_IS_FIXED = 0x30
-        if capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_NOT_SUPPORTED:
-            self.capabilities.append('hysteresis_not_supported')
-        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_READABLE:
-            self.capabilities.append('hysteresis_readable')
-        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_READ_AND_SETTABLE:
-            self.capabilities.append('hysteresis_read_and_setable')
-        elif capabilities & HYSTERESIS_MASK == HYSTERESIS_IS_FIXED:
-            self.capabilities.append('hysteresis_fixed')
-        # sensor threshold support
-        THRESHOLD_MASK = 0x30
-        THRESHOLD_IS_NOT_SUPPORTED = 0x00
-        THRESHOLD_IS_READABLE = 0x10
-        THRESHOLD_IS_READ_AND_SETTABLE = 0x20
-        THRESHOLD_IS_FIXED = 0x30
-        if capabilities & THRESHOLD_MASK == THRESHOLD_IS_NOT_SUPPORTED:
-            self.capabilities.append('threshold_not_supported')
-        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_READABLE:
-            self.capabilities.append('threshold_readable')
-        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_READ_AND_SETTABLE:
-            self.capabilities.append('threshold_read_and_setable')
-        elif capabilities & THRESHOLD_MASK == THRESHOLD_IS_FIXED:
-            self.capabilities.append('threshold_fixed')
-        # sensor event message control support
-        if (capabilities & 0x03) is 0:
-            pass
-        if (capabilities & 0x03) is 1:
-            pass
-        if (capabilities & 0x03) is 2:
-            pass
-        if (capabilities & 0x03) is 3:
-            pass
+        self._decode_capabilities(buffer.pop_unsigned_int(1))
 
         self.sensor_type_code = buffer.pop_unsigned_int(1)
         self.event_reading_type_code = buffer.pop_unsigned_int(1)
