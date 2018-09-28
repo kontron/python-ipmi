9ff932b4b4c34e271db48121c87e65496b84ff45
Heiko Thiery <heiko.thiery@kontron.com>
2018-09-28 11:29:31 +0200

    remove unneeded future import
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- pyipmi/ipmitool.py
+++ pyipmi/ipmitool.py
@@ -17,9 +17,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
-
 
 import sys
 import getopt
