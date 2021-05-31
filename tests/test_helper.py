4c11b956aa03cf2f388239fcd6e3b5e3552831f4
Heiko Thiery <heiko.thiery@gmail.com>
2021-05-31 08:37:32 +0200

    helper: rename REPOSITOY constants
    Add prefix REPOSITORY_.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -4,9 +4,11 @@
 from mock import MagicMock, call
 from nose.tools import eq_
 
-from pyipmi.helper import (clear_repository_helper, ERASURE_COMPLETED,
-                           ERASURE_IN_PROGRESS, INITIATE_ERASE,
-                           GET_ERASE_STATUS)
+from pyipmi.helper import (clear_repository_helper,
+                           REPOSITORY_ERASURE_COMPLETED,
+                           REPOSITORY_ERASURE_IN_PROGRESS,
+                           REPOSITORY_INITIATE_ERASE,
+                           REPOSITORY_GET_ERASE_STATUS)
 
 
 def test_clear_repository_helper():
@@ -15,17 +17,17 @@ def test_clear_repository_helper():
 
     clear_fn = MagicMock()
     clear_fn.side_effect = [
-        ERASURE_COMPLETED,
-        ERASURE_IN_PROGRESS,
-        ERASURE_COMPLETED,
+        REPOSITORY_ERASURE_COMPLETED,
+        REPOSITORY_ERASURE_IN_PROGRESS,
+        REPOSITORY_ERASURE_COMPLETED,
     ]
 
     clear_repository_helper(reserve_fn, clear_fn)
 
     clear_calls = [
-        call(INITIATE_ERASE, 0x1234),
-        call(GET_ERASE_STATUS, 0x1234),
-        call(GET_ERASE_STATUS, 0x1234),
+        call(REPOSITORY_INITIATE_ERASE, 0x1234),
+        call(REPOSITORY_GET_ERASE_STATUS, 0x1234),
+        call(REPOSITORY_GET_ERASE_STATUS, 0x1234),
     ]
     clear_fn.assert_has_calls(clear_calls)
     eq_(clear_fn.call_count, 3)
