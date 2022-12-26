6c27f6f9a9ad96ada8ce75e4de4599b8a68bea29
Heiko Thiery <heiko.thiery@gmail.com>
2022-12-26 19:32:17 +0100

    setup/tests/ add python 3.11
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(name=name,
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       entry_points={
