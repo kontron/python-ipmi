17decdba222e816febb290a1eae205dbe596ddb5
canteuni <21243338+canteuni@users.noreply.github.com>
2023-06-29 14:45:52 +0200

    tests: add check_completion_code unit tests
    Signed-off-by: canteuni <21243338+canteuni@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,11 @@ import pytest
 
 from array import array
 
-from pyipmi.utils import ByteBuffer, chunks
-from pyipmi.errors import DecodingError
+import pyipmi.msgs.device_messaging
+from pyipmi.utils import (ByteBuffer, chunks, check_completion_code,
+                          check_rsp_completion_code)
+from pyipmi.errors import DecodingError, CompletionCodeError
+from pyipmi.msgs import decode_message
 
 
 def test_bytebuffer_init_from_list():
@@ -118,3 +121,48 @@ def test_chunks():
         result.extend(chunk)
         assert len(chunk) == 2
     assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_check_completion_code_ok():
+    check_completion_code(0)
+
+
+def test_check_specific_completion_code_ok():
+    rsp = pyipmi.msgs.device_messaging.SetUserPasswordRsp()
+    decode_message(rsp, b'\x00')
+    check_rsp_completion_code(rsp)
+
+
+def test_check_completion_code_desc():
+    with pytest.raises(CompletionCodeError) as ex:
+        check_completion_code(0xc1)
+    assert ex.value.cc == 0xc1
+    assert ex.value.cc_desc == "Invalid Command"
+
+
+def test_check_completion_code_unknown_desc():
+    with pytest.raises(CompletionCodeError) as ex:
+        check_completion_code(0x81)
+    assert ex.value.cc == 0x81
+    assert ex.value.cc_desc == "Unknown error description"
+
+
+def test_check_rsp_completion_code_desc():
+    rsp = pyipmi.msgs.device_messaging.SetUserPasswordRsp()
+    decode_message(rsp, b'\x81')
+    with pytest.raises(CompletionCodeError) as ex:
+        check_rsp_completion_code(rsp)
+    assert rsp.cmdid == 71
+    assert rsp.netfn == 6 | 1
+    assert rsp.group_extension is None
+    assert ex.value.cc == 0x81
+    assert ex.value.cc_desc == "password test failed. Wrong password size was used."
+
+
+def test_check_rsp_completion_code_unknown_desc():
+    rsp = pyipmi.msgs.device_messaging.SetUserPasswordRsp()
+    decode_message(rsp, b'\x42')
+    with pytest.raises(CompletionCodeError) as ex:
+        check_rsp_completion_code(rsp)
+    assert ex.value.cc == 0x42
+    assert ex.value.cc_desc == "Unknown error description"
