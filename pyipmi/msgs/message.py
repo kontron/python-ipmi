f1acc29426e750c8051f33899ce5629d96925dc4
Heiko Thiery <heiko.thiery@gmail.com>
2024-10-28 08:53:12 +0100

    remove legacy "from __future__ import absolute_import"
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- pyipmi/msgs/message.py
+++ pyipmi/msgs/message.py
@@ -14,9 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-
-from __future__ import absolute_import
-
 from array import array
 
 from . import constants
